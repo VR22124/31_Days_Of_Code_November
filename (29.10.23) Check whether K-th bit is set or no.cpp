7f169e43573eb97286dfe672d85914a904112652
VR22124 <117558148+VR22124@users.noreply.github.com>
2023-11-01 21:53:54 +0530

    Update (29.10.23) Check whether K-th bit is set or no.cpp

--- (29.10.23) Check whether K-th bit is set or no.cpp
+++ (29.10.23) Check whether K-th bit is set or no.cpp
@@ -1,4 +1,4 @@
-(29/10/23) Check whether K-th bit is set or not
+/* (29/10/23) Check whether K-th bit is set or not
 
 Given a number N and a bit number K, check if Kth index bit of N is set or not. A bit is called set if it is 1. Position of set bit '1' should be indexed starting with 0 from LSB side in binary representation of the number.
 Note: Index is starting from 0. You just need to return true or false, driver code will take care of printing "Yes" and "No".
@@ -21,7 +21,7 @@ K = 2
 Output: 
 Yes
 Explanation: 
-Binary representation of 4 is 100, in which 2nd index bit from LSB is set. So, return true.
+Binary representation of 4 is 100, in which 2nd index bit from LSB is set. So, return true. */
 
 Solution:
 
