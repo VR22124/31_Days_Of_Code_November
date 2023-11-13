210dcbeb67d45107101e871273266c83e5b73d75
VISHNU ROHITH B <rohith.skct.ece@gmail.com>
2023-11-13 17:54:33 +0530

    Add files via upload
    Signed-off-by: VISHNU ROHITH B  <rohith.skct.ece@gmail.com>

---
+++ GFG_POTDa"/(13.11.23) Shortest Common Supersequence.java
@@ -0,0 +1,55 @@
+/* Shortest Common Supersequence
+
+Given two strings X and Y of lengths m and n respectively, find the length of the smallest string which has both, X and Y as its sub-sequences.
+Note: X and Y can have both uppercase and lowercase letters.
+
+Example 1
+
+Input:
+X = abcd, Y = xycd
+Output: 6
+Explanation: Shortest Common Supersequence
+would be abxycd which is of length 6 and
+has both the strings as its subsequences.
+Example 2
+
+Input:
+X = efgh, Y = jghi
+Output: 6
+Explanation: Shortest Common Supersequence
+would be ejfghi which is of length 6 and
+has both the strings as its subsequences. */
+
+Solution :
+
+class Solution
+{
+    //Function to find length of shortest common supersequence of two strings.
+    public static int shortestCommonSupersequence(String X,String Y,int m,int n)
+    {
+        return m+n-lcs(m,n,X,Y);
+    }
+    static int lcs(int m, int n , String s1,String s2)
+    {
+        int dp[][]=new int [m+1][n+1];
+        for(int i=0;i<=m;i++)
+        {
+            for(int j=0;j<=n;j++)
+            {
+                if(i==0||j==0)
+                {
+                    dp[i][j]=0;
+                }
+                else if(s1.charAt(i-1)==s2.charAt(j-1))
+                {
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else
+                {
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
